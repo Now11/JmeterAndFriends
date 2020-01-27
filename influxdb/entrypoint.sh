83bdd4dcd38b9b0fb5b4a929b8a24829b0270209
Denys Kuznietsov <Denys@domain-u.com>
2020-01-27 14:45:06 +0200

    added

---
+++ influxdb/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+if [ ! -f "/var/lib/influxdb/.init" ]; then
+    exec influxd $@ &
+
+    until wget -q "http://127.0.0.1:8086/ping" 2> /dev/null; do
+        sleep 1
+    done
+
+    influx -host=127.0.0.1 -port=8086 -execute="CREATE USER ${INFLUX_USER} WITH PASSWORD '${INFLUX_PASSWORD}' WITH ALL PRIVILEGES"
+    influx -host=127.0.0.1 -port=8086 -execute="CREATE DATABASE ${INFLUX_DB}"    
+    
+    touch "/var/lib/influxdb/.init"
+
+    kill -s TERM %1
+fi
+
+exec influxd $@
